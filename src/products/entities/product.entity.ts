faee4b3ab3664cb0a35020e301d9126b317171f8
wilson capacho <wiston.98@hotmail.com>
2026-08-23 17:08:47 -0500

    entidad sin relaciones

--- src/products/entities/product.entity.ts
+++ src/products/entities/product.entity.ts
@@ -2,11 +2,40 @@ import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
 
 @Entity()
 export class Product {
-  @PrimaryGeneratedColumn()
+  @PrimaryGeneratedColumn('uuid')
   id!: string;
 
   @Column('text', {
     unique: true,
   })
   title!: string;
+
+  @Column('numeric', {
+    default: 0,
+  })
+  price!: number;
+
+  @Column({
+    type: 'text',
+    nullable: true,
+  })
+  description!: string;
+
+  @Column('text', {
+    unique: true,
+  })
+  slug!: string;
+
+  @Column('int', {
+    default: 0,
+  })
+  stock!: number;
+
+  @Column('text', {
+    array: true,
+  })
+  sizes!: string[];
+
+  @Column('text')
+  gender!: string;
 }
