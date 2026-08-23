3b5de87768b1a4aad408282684c8fff7acafae8f
wilson capacho <wiston.98@hotmail.com>
2026-08-23 18:30:11 -0500

    crear slung con beforeInsert

--- src/products/entities/product.entity.ts
+++ src/products/entities/product.entity.ts
@@ -1,4 +1,4 @@
-import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+import { BeforeInsert, BeforeUpdate, Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
 
 @Entity()
 export class Product {
@@ -38,4 +38,17 @@ export class Product {
 
   @Column('text')
   gender!: string;
+
+  @BeforeInsert()
+  checkSlugInsert() {
+    if (!this.slug) {
+      this.slug = this.title;
+    }
+    this.slug = this.title
+      .toLowerCase()
+      .replaceAll(' ', '_')
+      .replaceAll("'", '');
+  }
+
+  //   @BeforeUpdate
 }
