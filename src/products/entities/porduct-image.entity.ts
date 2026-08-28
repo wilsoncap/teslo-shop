9c288c3648b95539f08c9cde99a105c672fd166a
wilson capacho <wiston.98@hotmail.com>
2026-08-27 21:24:38 -0500

    insertar de forma masiva y otros ajustes

--- src/products/entities/porduct-image.entity.ts
+++ src/products/entities/porduct-image.entity.ts
@@ -1,7 +1,7 @@
 import { Column, Entity, ManyToOne, PrimaryGeneratedColumn } from 'typeorm';
 import { Product } from './product.entity';
 
-@Entity()
+@Entity({name: 'products_images'})
 export class ProductImage {
   @PrimaryGeneratedColumn()
   id: number;
--- src/seed/seed.service.ts
+++ src/seed/seed.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { ProductsService } from '../products/products.service';
+import { initialData } from './data/seed-data';
 
 @Injectable()
 export class SeedService {
@@ -12,6 +13,16 @@ export class SeedService {
 
   private async insertNewProducts() {
     await this.productService.deleteAllProduts();
+
+    const products = initialData.products;
+
+    const insertPromises: Promise<unknown>[] = [];
+
+    products.forEach((product) => {
+      insertPromises.push(this.productService.create(product));
+    });
+
+    const results = await Promise.all(insertPromises);
 
     return true;
   }
