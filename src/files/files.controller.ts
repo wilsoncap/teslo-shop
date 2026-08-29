ad1817c7f3e4f0ff0f5d4ef8e2e40c404ee607a8
wilson capacho <wiston.98@hotmail.com>
2026-08-28 22:53:16 -0500

    subir archivos de manera controlada

--- src/files/files.controller.ts
+++ src/files/files.controller.ts
@@ -1,12 +1,29 @@
-import { BadRequestException, Controller, ParseFilePipe, Post, UploadedFile, UseInterceptors } from '@nestjs/common';
+import { BadRequestException, Controller, Get, Param, Post, Res, UploadedFile, UseInterceptors } from '@nestjs/common';
 import { FilesService } from './files.service';
 import { FileInterceptor } from '@nestjs/platform-express';
 import { fileFilter } from './helpers/fileFilter.helper';
 import { diskStorage } from 'multer';
+import type { Response } from 'express';
 
 @Controller('files')
 export class FilesController {
   constructor(private readonly filesService: FilesService) {}
+
+  @Get('product/:imageName')
+  findProductImage(
+    @Res() res: Response,
+    @Param('imageName') imageName: string,
+  ) {
+    const path = this.filesService.getStaticProductImage(imageName);
+    //return path;
+
+    /*res.status(403).json({
+      ok: false,
+      path: path,
+    });*/
+
+    res.sendFile(path);
+  }
 
   @Post('product')
   @UseInterceptors(FileInterceptor('file', {
@@ -22,7 +39,9 @@ export class FilesController {
     if (!file) {
        throw new BadRequestException('Make sure that file is an image');
     }
+
+    const secureUrl = `${file.originalname}`
     
-    return { fieldname: file.fieldname };
+    return { secureUrl };
   }
 }
--- src/files/files.service.ts
+++ src/files/files.service.ts
@@ -1,6 +1,18 @@
-import { Injectable } from '@nestjs/common';
+import { BadGatewayException, Injectable } from '@nestjs/common';
+import { existsSync } from 'fs';
+import { join } from 'path';
 
 @Injectable()
 export class FilesService {
   
+    getStaticProductImage(imageName: string){
+
+        const path = join(__dirname, '../../static/products', imageName)
+
+        if (!existsSync(path))
+            throw new BadGatewayException(`No product fund with image ${imageName}`)
+
+
+        return path;
+    }
 }
