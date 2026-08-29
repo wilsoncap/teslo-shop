17f313cd6da7011aa5bfe73a87069a6da6150bc5
wilson capacho <wiston.98@hotmail.com>
2026-08-29 16:02:27 -0500

    forma de desplegar archivo

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { ValidationPipe } from '@nestjs/common';
+import { Logger, ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-
+  const logger = new Logger('Boostrap');
   app.setGlobalPrefix('api');
 
   app.useGlobalPipes(
@@ -15,5 +15,6 @@ async function bootstrap() {
   );
 
   await app.listen(process.env.PORT ?? 3000);
+  logger.log(`App running on port ${process.env.PORT}`);
 }
 bootstrap();
